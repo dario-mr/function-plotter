b57136740028892472369b91fc2e58ee62928e13
dario-mr <dario.mauri9@gmail.com>
2026-06-21 15:13:18 +0200

    add prefix to production build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/function-plotter/" : "/",
   plugins: [react(), tailwindcss()],
-});
+}));
